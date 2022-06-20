7640a96340fbd3fe07c59ccd1817c86745e9172e
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-06-20 19:33:53 +0200

    Enable LTO for all keymaps

--- user/rules.mk
+++ user/rules.mk
@@ -7,6 +7,11 @@ NKRO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 
+# Enables Link Time Optimization (LTO) when compiling the keyboard. This makes
+# the process take longer, but it can significantly reduce the compiled size
+# (and since the firmware is small, the added time is not noticeable).
+LTO_ENABLE = yes
+
 # Additional files
 SRC += trekdemo.c
 
