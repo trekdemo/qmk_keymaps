74d40b24887194ec409f5038a748c9bb607c08f2
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-06-20 19:12:06 +0200

    Document userspace rule file

--- user/rules.mk
+++ user/rules.mk
@@ -1,7 +1,18 @@
+# Userspace - sharing code between keymaps
+# https://docs.qmk.fm/#/feature_userspace?id=rulesmk
+
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 NKRO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
 
+# Additional files
 SRC += trekdemo.c
+
+# # You can define RGB_ENABLE in your keymap’s rules.mk and then check for the
+# # variable in your userspace’s rules.mk
+# ifdef RGB_ENABLE
+#   # Include my fancy rgb functions source here
+#   SRC += cool_rgb_stuff.c
+# endif
