ecb041c48a05dc5acd8142ccad6f5cec93b7c7d7
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-06-21 21:55:31 +0200

    Format the shared keymap

--- user/trekdemo.h
+++ user/trekdemo.h
@@ -3,7 +3,6 @@
 
 #include QMK_KEYBOARD_H
 
-#define U_REDO  LCMD(KC_Z)
 #define U_PASTE LCMD(KC_V)
 #define U_COPY  LCMD(KC_C)
 #define U_CUT   LCMD(KC_X)
@@ -14,31 +13,15 @@
 #define CMDLCBR  LCMD(KC_LCBR)
 #define CMDRCBR  LCMD(KC_RCBR)
 
-// == Home-row Mod helpers =====================================================
-//  .-----------------------.    .-----------------------.
-//  | Alt | Ctl | Cmd | Sft |    | Sft | Cmd | Ctl | Alt |
-//  '-----------------------'    '-----------------------'
-#define ______________MOD_L2_L____________  KC_LOPT, KC_LCTL, KC_LCMD, KC_LSFT
-#define ______________MOD_L2_R____________  KC_RSFT, KC_RCMD, KC_RCTL, KC_ROPT
-
-
-// -- Thumb keys ---------------------------------------------------------------
-// .-----------------.   .-----------------.
-// | NUM | NAV | MOU |   |     | RSA | Hyp |
-// | Esc | Bsp | Tab |   | Ent | Spc |     |
-// '-----------------'   '-----------------'
-#define _________THUMB_L_________ LT(_NUM, KC_ESC), LT(_LWR, KC_BSPC) , LT(_MOUSE, KC_TAB)
-#define _________THUMB_R_________          KC_ENT , LT(_RAISE, KC_SPC), KC_HYPR
-
 // These are helpers to define home-row mods
-#define FL5(kc) LALT_T(kc)
-#define FL4(kc) LCTL_T(kc)
-#define FL3(kc) LCMD_T(kc)
-#define FL2(kc) LSFT_T(kc)
+#define LALT__A LALT_T(KC_A)
+#define LCTRL_R LCTL_T(KC_R)
+#define LCMD__S LCMD_T(KC_S)
+#define LSHFT_T LSFT_T(KC_T)
 
-#define FR2(kc) RSFT_T(kc)
-#define FR3(kc) RCMD_T(kc)
-#define FR4(kc) RCTL_T(kc)
-#define FR5(kc) RALT_T(kc)
+#define RSHFT_N RSFT_T(KC_N)
+#define RCMD__E RCMD_T(KC_E)
+#define RCTRL_I RCTL_T(KC_I)
+#define RALT__O RALT_T(KC_O)
 
 #endif
