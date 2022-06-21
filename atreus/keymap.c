108a3d621aecd636e099c52b217334b9270d420e
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-06-21 19:49:59 +0200

    Share the combos between keyboards

--- atreus/keymap.c
+++ atreus/keymap.c
@@ -62,13 +62,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-const uint16_t PROGMEM topLeft[]     = {KC_W, KC_F, COMBO_END};
-const uint16_t PROGMEM topRight[]    = {KC_U, KC_Y, COMBO_END};
-const uint16_t PROGMEM bottomRight[] = {KC_COMM, KC_DOT, COMBO_END};
-const uint16_t PROGMEM bottomLeft[]  = {KC_X, KC_C, COMBO_END};
-combo_t key_combos[COMBO_COUNT] = {
-    COMBO(topLeft, KC_TAB),
-    COMBO(topRight, KC_BSPC),
-    COMBO(bottomLeft, KC_ESC),
-    COMBO(bottomRight, OSM(MOD_LSFT)),
-};
