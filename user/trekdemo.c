5a3dddd9972fad55f4324d461ec9280dc55105cb
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2022-10-11 09:15:37 +0200

    Put ESC on the Sym layer for easy access
    ...and experimentation.

--- user/trekdemo.c
+++ user/trekdemo.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_SYM] = LAYOUT_split_3x5_3(
-    XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_TILD,   KC_AMPR, KC_LPRN, KC_RPRN, KC_PERC, KC_EQL ,
+    KC_ESC , XXXXXXX, XXXXXXX, KC_BSLS, KC_TILD,   KC_AMPR, KC_LPRN, KC_RPRN, KC_PERC, KC_EQL ,
     KC_EXLM, KC_AT  , KC_UNDS, KC_COLN, KC_DLR ,   KC_PIPE, KC_LBRC, KC_RBRC, KC_PLUS, KC_MINS,
     KC_CIRC, XXXXXXX, KC_HASH, KC_SCLN, KC_GRV ,   XXXXXXX, KC_LCBR, KC_RCBR, KC_ASTR, KC_SLSH,
                       _______, _______, _______,   _______, _______, _______
